2880a520d06b0cb5104637bbf3c8405a1c3cc261
Artem Bilko <Art.Bilko14@gmail.com>
2024-10-13 22:18:20 +0300

    UPD: Основні структури даних

--- task2.py
+++ task2.py
@@ -1,17 +1,17 @@
-from collections import deque;
+from collections import deque
 
 def is_palindrome(word):
-    dequeue = deque();
+    dequeue = deque()
     
     for char in list(word.lower().strip()):
-        dequeue.append(char);
+        dequeue.append(char)
     
     while len(dequeue) > 1:
-        if (dequeue.pop() != dequeue.popleft()):
-            return False;
-        return True;
+        if dequeue.pop() != dequeue.popleft():
+            return False
+        return True
     
-    return False;
+    return False
     
 word = 'корок'
 
